8600a07e8f8082609813daae92e0c535a63b22ff
assareh <assareh@hashicorp.com>
2020-08-26 10:21:43 -0700

    initial

--- remote_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "multicloud-provisioning-demo"
-    workspaces {
-      name = "azurerm-issue"
-    }
-  }
-}
